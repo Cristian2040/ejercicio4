431579a62887e7659cf2874a6b588b091b9f3503
Cristian2040 <168566901+Cristian2040@users.noreply.github.com>
2024-07-24 12:28:49 -0600

    Update index.js

--- ejercicio4/index.js
+++ ejercicio4/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 var path = require("path");
-var usuariosRutas=require("./routes/usuariosRutas");
+var usuariosRutas = require("./routes/usuariosRutas");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use("/",express.static(path.join(__dirname,"/web")));
-app.use(express.urlencoded({extended:true}));
-app.use("/",usuariosRutas);
+app.use("/", express.static(path.join(__dirname, "/web")));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usuariosRutas);
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{
-    console.log("Servidor en http://localhost:"+port);
+app.listen(port, () => {
+    console.log("Servidor en http://localhost:" + port);
 });
