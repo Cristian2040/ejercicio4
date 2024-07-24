92af86a4c51b82499386f345ba7511ef63a7d40c
Cristian2040 <168566901+Cristian2040@users.noreply.github.com>
2024-07-24 11:52:59 -0600

    Update ConectarBD.js

--- ejercicio4/bd/ConectarBD.js
+++ ejercicio4/bd/ConectarBD.js
@@ -1,37 +1,35 @@
 require('dotenv').config();
-class ConectarBD{
-    constructor(){
-        this.conexion=null;
-        this.mysql=require("mysql2");
+class ConectarBD {
+    constructor() {
+        this.conexion = null;
+        this.mysql = require("mysql2/promise");
     }
-    async conectarMySql(){
+
+    async conectarMySql() {
         try {
-            this.conexion=await this.mysql.createConnection({
-                host:process.env.host,
-                user:process.env.user,
-                password:process.env.password,
-                database:process.env.database,
-                port:process.env.port
+            this.conexion = await this.mysql.createConnection({
+                host: process.env.host,
+                user: process.env.user,
+                password: process.env.password,
+                database: process.env.database,
+                port: process.env.port
             });
             console.log("Conexión creada a MySql");
-        
+            return this.conexion;
         } catch (error) {
-            console.error("Error al conectar con MySql "+error);
+            console.error("Error al conectar con MySql " + error);
+            throw error;
         }
     }
-    async cerrarConexion(){
+
+    async cerrarConexion() {
         try {
             await this.conexion.end();
             console.log("Desconexión de MySql");
         } catch (error) {
-            console.error("Error al desconectar de MySql "+error);
+            console.error("Error al desconectar de MySql " + error);
         }
     }
 }
 
-async function principal() {
-    var conectarbd=new ConectarBD();
-    await conectarbd.conectarMySql();
-    await conectarbd.cerrarConexion();
-}
-principal();
+module.exports = new ConectarBD();
